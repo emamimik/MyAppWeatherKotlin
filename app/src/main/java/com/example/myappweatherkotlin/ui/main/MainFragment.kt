db4ceaa6a34ba74b0b2eb7e0d191d9471a3cb134
emamimik <86338868+emamimik@users.noreply.github.com>
2021-10-11 14:00:03 +0300

    hw2

--- app/src/main/java/com/example/myappweatherkotlin/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/myappweatherkotlin/ui/main/MainFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.myappweatherkotlin.AppState
 import com.example.myappweatherkotlin.R
 
 class MainFragment : Fragment() {
@@ -28,5 +29,30 @@ class MainFragment : Fragment() {
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
     }
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        viewModel.getLiveData().observe(viewLifecycleOwner, Observer { renderData(it)
+        })
+        viewModel.getWeather()
+    }
+    private fun renderData(appState: AppState) {
+        when (appState) {
+            is AppState.Success -> {
+                val weatherData = appState.weatherData
+                loadingLayout.visibility = View.GONE
+                Snackbar.make(mainView, "Success", Snackbar.LENGTH_LONG).show()
+            }
+            is AppState.Loading -> {
+                © geekbrains.ru 20
+                loadingLayout.visibility = View.VISIBLE
+            }
+            is AppState.Error -> {
+                loadingLayout.visibility = View.GONE
+                Snackbar
+                    .make(mainView, "Error", Snackbar.LENGTH_INDEFINITE)
+                    .setAction("Reload") { viewModel.getWeather() }
+                    .show()
+            }
 
 }
